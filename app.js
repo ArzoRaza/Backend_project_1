982d1dacc4105e7be9a10afa501378113d68966f
ArzoRaza <ccoding76@gmail.com>
2024-06-15 23:05:09 +0530

    initial setup

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("Hello arzo let build scratch");
+});
+
+app.listen(3000);
