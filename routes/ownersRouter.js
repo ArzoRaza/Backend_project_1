af3fe4992cad3b62e8dd4d2a85d4482df5fff3ad
ArzoRaza <ccoding76@gmail.com>
2024-06-19 08:47:53 +0530

    Some changes

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 const ownerModel = require("../models/owner-model");
 
-router.get("/", function (req, res){
-    res.send("hey it's working (owner)");
-});
-
 require('dotenv').config();
 if(process.env.NODE_ENV === "development"){
     router.post("/create", function (req, res){
         res.send("hey it's working ");
     });
 };
+
+
+router.get("/", function (req, res){
+    res.send("hey it's working (owner)");
+});
+
+
 
 
 module.exports = router;
